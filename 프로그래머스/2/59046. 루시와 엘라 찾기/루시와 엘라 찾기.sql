33e708ce378f30f4ebb540f5d7c8db4f9d74fbaf
이희수 <leehsdd33@gmail.com>
2024-12-27 18:12:39 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,4 @@
+-- 코드를 입력하세요
+SELECT animal_id, name, SEX_UPON_INTAKE
+from animal_ins
+where name = 'Lucy' or name = 'Ella' or name = 'Pickle' or name = 'Rogan' or name = 'Sabrina' or name = 'Mitty'
