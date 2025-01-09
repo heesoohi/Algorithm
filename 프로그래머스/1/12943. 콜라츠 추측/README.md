1f7eef4e96af9d384e27b78180d94114ef7e7a49
이희수 <leehsdd33@gmail.com>
2025-01-09 09:45:56 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/README.md
+++ 프로그래머스/1/12943. 콜라츠 추측/README.md
@@ -1,10 +1,10 @@
 # [level 1] 콜라츠 추측 - 12943 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12943#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12943) 
 
 ### 성능 요약
 
-메모리: 85.3 MB, 시간: 0.06 ms
+메모리: 76 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 09일 09:44:08
+2025년 01월 09일 09:45:54
 
 ### 문제 설명
 
--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -4,11 +4,10 @@ class Solution {
         
         while (num != 1) {
             if (answer == 500){
-                answer = -1;
-                break;
+                return -1;
             } else{
                 if (num%2 ==0){
-                    num = num / 2;
+                    num/= 2;
                 } else {
                     num = num * 3 + 1;
                 }
