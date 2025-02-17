41cd7ce012926cc9dcb8bfea1fb47b91bf7c70fe
이희수 <leehsdd33@gmail.com>
2025-02-17 09:37:54 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 66.39 ms, Memory: 99 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Set<String> runners = new HashSet<>();
+        
+        for (String runner: participant) {
+            if (!runners.contains(runner)) {
+                runners.add(runner);
+            } else {
+                runners.remove(runner);
+            }
+        }
+        
+        for (String runner: completion) {
+            if (runners.contains(runner)) {
+                runners.remove(runner);
+            } else {
+                runners.add(runner);
+            }
+        }
+        
+        for (String ans: runners) {
+            return ans;
+        }
+        return null;
+    }
+}
